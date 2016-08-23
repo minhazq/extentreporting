5fbe9fbc01b2cff00977c1062988869b64a236d6
Minhajul Quraishi <mquraishi@Minhajuls-MacBook-Pro-2.local>
2016-08-23 11:15:26 -0400

    init setup

---
+++ src/main/java/mq/demo/extentreporting/App.java
@@ -0,0 +1,13 @@
+package mq.demo.extentreporting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
